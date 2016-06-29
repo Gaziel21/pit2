bcda790bcfb20e82eef988ba4a1e7ce76167122f
flora sanchez <sanchezcusif@gmail.com>
2016-06-29 17:43:58 -0500

    asha

--- src/main/java/pe/com/app/core/model/RolEnlaceModel.java
+++ src/main/java/pe/com/app/core/model/RolEnlaceModel.java
@@ -6,7 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "ROL_ENLACE")
 public class RolEnlaceModel implements Serializable {
